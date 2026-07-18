0e12a11e3b462bc1879e0caef3e38d4c212c0ba1
Caleb Barnard <poisonpsychicghost@gmail.com>
2026-07-18 14:56:44 -0400

    Update: Fixed missing fields in category.py

--- backend/models/category.py
+++ backend/models/category.py
@@ -1,12 +1,19 @@
 from sqlalchemy import Column, Integer, String, JSON
+from sqlalchemy.orm import relationship
 from db import Base
 
 class Category(Base):
     __tablename__ = "categories"
+
+    #fields
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
     style = Column(JSON)
     icon = Column(String)
     description = Column(String)
     metadata_keys = Column(JSON, default=dict)
+    
+    #relationships
+    location = relationship("InventoryLocation")
+    fooditem = relationship("FoodItem")
     
