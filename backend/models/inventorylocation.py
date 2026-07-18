3509737dcc1b0ec13e60991bcf112e245cd61543
Caleb Barnard <poisonpsychicghost@gmail.com>
2026-07-18 14:39:00 -0400

    Feat: Built Out Full inventorylocations model.

--- backend/models/inventorylocation.py
+++ backend/models/inventorylocation.py
@@ -1,7 +1,18 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from db import Base
 
 class InventoryLocation(Base):
     __tablename__ = "inventory_locations"
+    
+    #fields
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+    emoji = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+
+    #relationships
+    category = relationship("Category")
+    fooditem = relationship("FoodItem")
+
