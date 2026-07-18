2423262ef83d03d0f43931f0540a2e770d8486ac
Caleb Barnard <poisonpsychicghost@gmail.com>
2026-07-18 14:52:07 -0400

    Feat: fully built out category model

--- backend/models/category.py
+++ backend/models/category.py
@@ -1,7 +1,12 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, JSON
 from db import Base
 
 class Category(Base):
     __tablename__ = "categories"
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
+    style = Column(JSON)
+    icon = Column(String)
+    description = Column(String)
+    metadata_keys = Column(JSON, default=dict)
+    
